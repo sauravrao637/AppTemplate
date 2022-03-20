d887379879ef10f9004d23e5a0e66ccd7f733e8a
camo0112 <56369484+camo0112@users.noreply.github.com>
2022-03-20 12:02:26 +0530

    Added Idle Status in Resource class

--- app/src/main/java/com/camo/template/util/Resource.kt
+++ app/src/main/java/com/camo/template/util/Resource.kt
@@ -12,12 +12,16 @@ data class Resource<out T>(val status: Status, val data: T?, val errorInfo: Stri
         fun <T> loading(data: T?): Resource<T> =
             Resource(status = Status.LOADING, data = data, errorInfo = null)
 
-        fun<T> loading(): Resource<T> = Resource(status = Status.LOADING, data = null, errorInfo = null)
+        fun <T> loading(): Resource<T> =
+            Resource(status = Status.LOADING, data = null, errorInfo = null)
+
+        fun <T> idle(): Resource<T> = Resource(status = Status.IDLE, null, null)
     }
 }
 
 enum class Status {
     SUCCESS,
     ERROR,
-    LOADING
+    LOADING,
+    IDLE
 }
